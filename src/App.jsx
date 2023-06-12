d90cf9dd2a599494cdeedbcbdf025f0f7360fa19
Nabil <nallis014@gmail.com>
2023-06-12 11:06:09 -0300

    arreglo en initial state y responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react'
 import { getDollar } from './services/getDollar'
 
 function App () {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState();
   const [dollar, setDollar] = useState()
 
   useEffect(() => {
@@ -21,6 +21,13 @@ function App () {
     getCotizacion()
   }, [])
 
+  const onChange = (value) => {
+
+    if (value < 0) return
+    setAmount(value)
+
+  }
+
   return (
     <main >
       <section className='absolute top-0 left-0 z-1' >
@@ -29,24 +36,24 @@ function App () {
       <section className='z-40 flex relative h-screen w-screen items-center justify-center'>
         <section className='grid md:flex bg-white gap-4 p-4 rounded-3xl shadow-xl'>
           <div className='md:flex-[0.75]'>
-            <Calculator amount={amount} onChange={value => setAmount(value)} />
+            <Calculator amount={amount} onChange={onChange} />
           </div>
-          <div className='grid md:flex-1 bg-emerald-600 rounded-3xl'>
-            <ul className='flex  flex-col gap-4'>
+          <div className='grid md:flex-1 md:min-w-[550px]  items-center  min-h-[300px] bg-emerald-600 rounded-3xl'>
+            <ul className='flex flex-col gap-4'>
               {
                 dollar?.map(({ nombre, venta }) => {
                   const total = amount ? Number(amount / venta) : venta
                   return (
                     <li key={nombre} className='flex text-md items-center gap-6 md:gap-14 justify-between px-4 py-2'>
-                      <div className='text-emerald-100 font-medium'>{nombre}</div>
+                      <div className='text-emerald-100 max-w-[125px] md:max-w-lg font-medium'>{nombre}</div>
                       <div className='text-3xl flex items-center gap-4'>
                         {
-                          amount ? (<div className='font-bold font-mono text-emerald-400 text-lg md:text-xl'>{Number(total).toLocaleString('es-AR', {
+                          amount ? <div className='font-bold font-mono text-emerald-400 text-lg md:text-xl'>{Number(total).toLocaleString('es-AR', {
                             style: 'currency',
                             currency: 'USD'
                           })
                           }
-                          </div>) : null
+                          </div> : null
                         }
                         <div className='font-bold font-mono text-emerald-300 text-2xl md:text-3xl'>{Number(venta).toLocaleString('es-AR', {
                           style: 'currency',
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,7 +6,7 @@ export default function Calculator ({ amount, onChange }) {
   return (
     <div className="w-full">
       <label className="block space-y-0 pb-4 px-4 font-mono text-lg" htmlFor="count">Monto en $ARG</label>
-      <input autoFocus value={amount} onChange={(e) => onChange(e.target.value)} id="count" type="number" className=" p-2 bg-white border-[1px] border-emerald-300 focus:outline-none focus:border-emerald-700 focus:ring-emerald-700 block w-full rounded-full focus:ring-2 font-mono px-5 text-right text-3xl " />
+      <input autoFocus value={amount} placeholder="0" onChange={(e) => onChange(e.target.value)} id="count" type="number" className=" p-2 bg-white border-[1px] border-emerald-300 focus:outline-none focus:border-emerald-700 focus:ring-emerald-700 block w-full rounded-full focus:ring-2 font-mono px-5 text-right text-3xl " />
     </div>
   )
 }
